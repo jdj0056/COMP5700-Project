d2ec078f5c101ed87705b1766032c2a6dbd2537d
Josh <jdj0056@auburn.edu>
2026-04-24 11:43:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import project_task1
+import project_task2
+import project_task3
+import os
+
+def run():
+    os.system("python project_task1.py")
+    os.system("python project_task2.py")
+    os.system("python project_task3.py")
+
+if __name__ == "__main__":
+    run()
